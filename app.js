7e9745994d6288ffb8fa758d8a28b3df5c22a160
Kenny Zhang <50647456+kennyzhang620@users.noreply.github.com>
2023-03-18 15:46:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -21,15 +21,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
+
+
 var fs = require('fs');
 var util = require('util');
-var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'a'});
-var log_stdout = process.stdout;
+var logFile = fs.createWriteStream('log.txt', { flags: 'a' });
+  // Or 'w' to truncate the file every time the process starts.
+var logStdout = process.stdout;
 
-console.log = function(d) { //
-  log_file.write(String(d) + '\n');
-  log_stdout.write(String(d) + '\n');
-};
+console.log = function () {
+  logFile.write(util.format.apply(null, arguments) + '\n');
+  logStdout.write(util.format.apply(null, arguments) + '\n');
+}
+console.error = console.log;
+
+
+
 
 const halfHr = 1000 * 60 * 30;
 var session;
