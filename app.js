54ebdc531fe5e9f31634b8c3538b57725fbd0046
kenny zhang <ktzhang@sfu.ca>
2023-03-18 21:46:40 -0700

    Migrate to mySQL and cPanel hosting

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const bodyParser = require('body-parser');
 const path = require('path')
 
 const PORT = process.env.PORT || 5010;
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname +'sfu-research-db/', 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
