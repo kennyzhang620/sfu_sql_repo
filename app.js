96d7c4dca0a24e863e02480c23348ffd22976e84
kenny zhang <ktzhang@sfu.ca>
2023-04-21 23:39:58 -0700

    temporary

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 var fs = require('fs');
 var util = require('util');
-var logFile = fs.createWriteStream('log.txt', { flags: 'w' });
+var logFile = fs.createWriteStream('log.txt', { flags: 'a' });
   // Or 'w' to truncate the file every time the process starts.
 var logStdout = process.stdout;
 
