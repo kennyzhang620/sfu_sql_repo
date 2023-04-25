d6036d730143ba8dab24ae7e6d4e3515794a4767
KTZHANG <kennyz620@hotmail.com>
2023-04-24 18:40:49 -0700

    optimizations

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ console.error = console.log;
 
 function hashCode(str) {
     let hash = 0;
-    for (let i = 0, len = str.length; i < len; i++) {
+    for (var i = 0, len = str.length; i < len; i++) {
         let chr = str.charCodeAt(i);
         hash = (hash << 5) - hash + chr;
         hash |= 0; // Convert to 32bit integer
