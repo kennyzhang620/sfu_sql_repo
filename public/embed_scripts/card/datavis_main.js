c6bcbc8e314707f6b57f1046eaaaa66677026b8e
KTZHANG <kennyz620@hotmail.com>
2023-04-28 10:56:52 -0700

    testing edits

--- public/embed_scripts/card/datavis_main.js
+++ public/embed_scripts/card/datavis_main.js
@@ -157,6 +157,8 @@ function clearFields() {
 	for (var i = 0; i < inputBars.length; i++) {
 		inputBars[i].value = "";	
 	}
+	
+	filter(inputBars[0].value, inputBars[1].value, inputBars[2].value, inputBars[3].value, inputBars[4].value, inputBars[5].value, inputBars[6].value, inputBars[7].value);
 }
 
 function init() {
