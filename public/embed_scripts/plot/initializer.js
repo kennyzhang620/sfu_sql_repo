0892f2f8076915c2e653830b3529cbb311b5a781
KTZHANG <kennyz620@hotmail.com>
2023-06-12 15:23:49 -0700

    test

--- public/embed_scripts/plot/initializer.js
+++ public/embed_scripts/plot/initializer.js
@@ -39,7 +39,7 @@ function init_main() {
                         <div id="val_left" class="sliderValue" data-index="0" value="10" style="float:left;">10</div>
                         <div id="val_right" class="sliderValue" data-index="1" value="90" style="float:left;transform:translate(-101%,400%);">90</div>
                     </div>
-                    <div id="main_slider" style="padding:8px;"><div id="slider-range" style=""></div></div>
+                    <div id="main_slider" style="padding:8px; transform: translate(30%, -13%);"><div id="slider-range" style=""></div></div>
                 </div>
                 <div id="section_entries" style="padding: 1px;">PLOT ENTRIES</div>
                 <div id="selector_entry" style="padding:8px;">
