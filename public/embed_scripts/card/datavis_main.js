a249d1fc20b8c9c944560ac37a24645b98678bb1
KTZHANG <kennyz620@hotmail.com>
2023-06-12 16:57:07 -0700

    add dropdown

--- public/embed_scripts/card/datavis_main.js
+++ public/embed_scripts/card/datavis_main.js
@@ -507,7 +507,7 @@ function filter(projectName, researchNames, piNames, copiNames, collabNames, fun
 		var coordsLat = parsedD[i].latitude;
 		var coordsLong = parsedD[i].longitude;
 
-		console.log("maint: ", Project.toLowerCase().includes(projectName?.toLowerCase().trim() ?? ""))
+		console.log("maint: ", Project.toLowerCase(), (projectName?.toLowerCase().trim() ?? ""))
 		if (Project.toLowerCase().includes(projectName?.toLowerCase().trim() ?? "") &&
 			site.toLowerCase().includes(researchNames?.toLowerCase().trim() ?? "") &&
 			PIs.toLowerCase().includes(piNames?.toLowerCase().trim() ?? "") &&
