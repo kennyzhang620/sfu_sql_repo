0f549852c1e86f922072f3896c707d45dca4c904
KTZHANG <kennyz620@hotmail.com>
2023-06-29 11:52:49 -0700

    fix arcs on map

--- public/embed_scripts/card/datavis_main.js
+++ public/embed_scripts/card/datavis_main.js
@@ -593,7 +593,7 @@ function filter(projectName, researchNames, piNames, copiNames, collabNames, fun
 loadSection();
 
 function getZoom(sqDist) {
-	console.log("vtr", 7*(1/sqDist))
+	console.log("vtr", minZoomV+7*(1/sqDist))
 	if (sqDist == 0) {
 		return minZoomV+7;
 	}
